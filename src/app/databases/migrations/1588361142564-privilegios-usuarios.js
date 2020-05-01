3fa99cc4524f294819e4a8fd9f10b875650aa4dc
Renan Zulian <renanzulian@gmail.com>
2020-05-01 16:57:25 -0300

    Primeira migrate funcionando

--- src/app/databases/migrations/1588361142564-privilegios-usuarios.js
+++ src/app/databases/migrations/1588361142564-privilegios-usuarios.js
@@ -1,9 +1,28 @@
+const postgres = require('../postgres.js')
+require('dotenv/config')
 'use strict'
 
-module.exports.up = function (next) {
+
+module.exports.up = async function (next) {
+  const query = `
+  CREATE TABLE IF NOT EXISTS privilegios_usuarios (
+    id_privilegio SERIAL PRIMARY KEY,
+    nome VARCHAR(30) UNIQUE NOT NULL,
+    criar_usuarios BOOLEAN NOT NULL,
+    editar_usuarios BOOLEAN NOT NULL,
+    desativar_usuarios BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+  )
+  `
+  await postgres.execute(query)
   next()
 }
 
-module.exports.down = function (next) {
+module.exports.down = async function (next) {
+  const query = `
+  DROP TABLE IF EXISTS privilegios_usuarios
+  `
+  await postgres.execute(query)
   next()
 }
