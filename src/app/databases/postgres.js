208642bc3e35602e9ddc0a995c0309521a9a07cc
Renan Zulian <renanzulian@gmail.com>
2020-05-01 16:57:09 -0300

    Configurando as variaveis no local também (para nao dar erro)

--- src/app/databases/postgres.js
+++ src/app/databases/postgres.js
@@ -4,9 +4,9 @@ require('dotenv/config')
 const pool = new Pool({
   host: process.env.SQL_HOST,
   port: process.env.SQL_PORT,
-  user: process.env.SQL_USERNAME,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE,
+  user: process.env.SQL_USERNAME || 'root',
+  password: process.env.SQL_PASSWORD || 'root',
+  database: process.env.SQL_DATABASE || 'node-forfun',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
