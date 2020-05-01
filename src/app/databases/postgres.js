ee4511694cff453ba7d15f5b5fb227e5ccdcaa29
Renan Zulian <renanzulian@gmail.com>
2020-05-01 16:22:59 -0300

    Configurando arquivo de conexão com o banco

---
+++ src/app/databases/postgres.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg')
+require('dotenv/config')
+
+const pool = new Pool({
+  host: process.env.SQL_HOST,
+  port: process.env.SQL_PORT,
+  user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+pool
+  .connect()
+  .then(() => {
+    console.log('Postgress connected.')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+module.exports = pool
